fd4f2aeb874105dadcbd4b5a217d8a9cb8aa3304
David Martinez Gil <davidmartinezgilp@gmail.com>
2026-03-22 16:24:06 -0400

    Add installation script for gommit on Windows

---
+++ install.ps1
@@ -0,0 +1,7 @@
+$url = "https://github.com/Bicheka/gommit/releases/latest/download/gommit-windows-x64.exe "
+$dest = "$HOME\AppData\Local\Microsoft\WindowsApps\gommit.exe"
+
+Write-Host "Downloading gommit..." -ForegroundColor Cyan
+Invoke-WebRequest -Uri $url -OutFile $dest
+
+Write-Host "Success! You can now use 'gommit' in any terminal." -ForegroundColor Green
