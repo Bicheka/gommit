c19996e915dfc804049d00c0fab0065b763498bd
David Martinez Gil <davidmartinezgilp@gmail.com>
2026-04-05 14:32:39 -0400

    Update binary names for gommit installation script

--- install-gommit.sh
+++ install-gommit.sh
@@ -7,11 +7,11 @@ ARCH="$(uname -m)"
 
 # Determine the binary name based on OS and ARCH
 if [[ "$OS" == "linux" && "$ARCH" == "x86_64" ]]; then
-    BIN="gommit-linux-x64"
+    BIN="gommit-linux-amd64"
 elif [[ "$OS" == "darwin" && "$ARCH" == "x86_64" ]]; then
-    BIN="gommit-darwin-x64"
+    BIN="gommit-macos-amd64"
 elif [[ "$OS" == "darwin" && "$ARCH" == "arm64" ]]; then
-    BIN="gommit-darwin-arm64"
+    BIN="gommit-macos-arm64"
 else
     echo "Unsupported OS/ARCH: $OS/$ARCH"
     exit 1
@@ -23,4 +23,4 @@ URL="https://github.com/bicheka/gommit/releases/download/$VERSION/$BIN"
 curl -Lo /usr/local/bin/gommit "$URL"
 chmod +x /usr/local/bin/gommit
 
-echo "Gommit installed! Run 'gommit --help'"
+echo "gommit installed! Run 'gommit --help'"
